4cd0cd9abc0f2a5d2108ec31115cda1f22b8f2fa
patbr21 <brpatrick1@gmail.com>
2020-10-22 22:16:06 +0200

    Update cachematrix.R
    Return a matrix that is the inverse of 'x'  or return the cached inverse of x.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
+## define the cache m# get the value of the vector
+## assign the input matrix y to the variable x in the parent environment
+## re-initialize m in the parent environment to null
+## return the matrix x
+## set the cache m equal to the inverse of the matrix x
+## return the cached inverse of x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL               
+        set <- function(y) {
+                x <<- y         
+                m <<- NULL      
+        }
+        get <- function() x     
+        setinverse <- function(inverse) m <<- inverse 
+        getinverse <- function() m 
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x', or get cahehd data, if you already have a matrix that is the inverse of x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
